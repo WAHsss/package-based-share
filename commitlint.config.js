1d3f7f357044bdaaf6f8b14dc272ff997f145248
Ahianwang <ahianwang@163.com>
2023-06-19 16:17:08 +0800

    feat: test 05

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = { 
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes','@commitlint/config-lerna-scopes'],
-  // ignores: [(commit) => commit.startsWith('Publish')],
+  ignores: [(commit) => commit.startsWith('Publish')],
 
 };
